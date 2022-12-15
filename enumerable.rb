e75af66ee5e0ab5ac4bd81f4f7e0f042a4c0fe48
Yilikal250 <teshomeyilikal250@gmail.com>
2022-12-15 23:25:54 +0300

    added enumerable module

---
+++ enumerable.rb
@@ -0,0 +1,23 @@
+module MyEnumerable
+  def all_method?
+    list.each do |e|
+      return false unless yield e
+    end
+    true
+  end
+
+  def any_method?
+    list.each do |e|
+      return true if yield e
+    end
+    false
+  end
+
+  def filter_method
+    result = []
+    list.each do |e|
+      result << e if yield e
+    end
+    result
+  end
+end
